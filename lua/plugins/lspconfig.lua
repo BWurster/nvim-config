4246791e3c6f783b46bd54beb71bd3ebaefd922a
Ben Wurster <wursterben17@gmail.com>
2024-03-25 14:28:03 -0500

    Added autosave and codeium

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,22 +1,33 @@
 return {
-  "neovim/nvim-lspconfig",
-  config = function()
-    local nvim_lsp = require("lspconfig")
-    nvim_lsp.clangd.setup({
-      cmd = { "clangd" },
-      filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
-      root_dir = nvim_lsp.util.root_pattern(
-        ".clangd",
-        ".clang-tidy",
-        ".clang-format",
-        "compile_commands.json",
-        "compile_flags.txt",
-        "configure.ac",
-        ".git"
-      ),
-      init_options = {
-        compilationDatabasePath = "./build",
-      },
-    })
-  end,
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local nvim_lsp = require("lspconfig")
+      nvim_lsp.clangd.setup({
+        cmd = { "clangd" },
+        filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
+        root_dir = nvim_lsp.util.root_pattern(
+          ".clangd",
+          ".clang-tidy",
+          ".clang-format",
+          "compile_commands.json",
+          "compile_flags.txt",
+          "configure.ac",
+          ".git"
+        ),
+        init_options = {
+          compilationDatabasePath = "./build",
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      lspconfig = require("lspconfig")
+      lspconfig.pyright.setup({
+        filetypes = { "python" },
+      })
+    end,
+  },
 }
--- lua/plugins/telescope.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'nvim-telescope/telescope.nvim', 
-    keys = {
-      {
-        {'<leader>fg', "<cmd>Telescope live_grep<cr>", desc = "Live grep"},
-        {'<leader>ff', "<cmd>Telescope find_files<cr>" desc = "Find file"},
-      },
-    },    
-}
