129fc9f6a5d29eec7b69297c45577dd926e8ae85
Ben Wurster <wursterben17@gmail.com>
2024-04-06 01:00:20 -0500

    total backup

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -3,6 +3,14 @@ return {
   opts = {
     ensure_installed = {
       "pyright",
+      "clangd",
+      "clang-format",
+      "cmake-language-server",
+      "gopls",
+      "rust-analyzer",
+      "typescript-language-server",
+      "vim-language-server",
+      "yaml-language-server",
     },
   },
 }
