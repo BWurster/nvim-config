5d7639c368ec1118e4c5cc228e9255c6d8318f01
Ben Wurster <wursterben17@gmail.com>
2024-04-06 01:02:32 -0500

    back to square one; we're doing this from scratch now

--- init.lua
+++ init.lua
@@ -1,2 +1,26 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+-- set up tab operations
+vim.o.expandtab = true -- Set expandtab (replace tab characters with spaces)
+vim.o.tabstop = 4 -- Set tabstop (number of spaces that a <Tab> in the file counts for)
+vim.o.softtabstop = 4 -- Set softtabstop (number of spaces to use for each step of (auto)indent)
+vim.o.shiftwidth = 4 -- Set shiftwidth (number of spaces used for each step of autoindent)
+vim.g.mapleader = " "
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+require("lazy").setup("plugins")
+
+
+
+vim.keymap.set('n', '<leader>n', ':Neotree filesystem reveal left<CR>', {})
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,26 +1,30 @@
-return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = {
-      "bash",
-      "html",
-      "javascript",
-      "json",
-      "lua",
-      "markdown",
-      "markdown_inline",
-      "python",
-      "query",
-      "regex",
-      "tsx",
-      "typescript",
-      "vim",
-      "yaml",
-      "cpp",
-      "rust",
-      "go",
-      "css",
-      "arduino",
-    },
+ return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function () 
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+                ensure_installed = {
+    "javascript",
+    "typescript",
+    "python",
+    "html",
+    "css",
+    "json",
+    "java",
+    "c",
+    "cpp",
+    "ruby",
+    "lua",
+    "go",
+    "rust",
+    "bash",
+    "markdown"
   },
-}
+          sync_install = false,
+          highlight = { enable = true },
+          indent = { enable = true },  
+        })
+    end
+    }
