29fa0e6387517d7d245a810d9096a0a0937f11df
Ben Wurster <wursterben17@gmail.com>
2024-04-07 23:47:46 -0500

    lsp support

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -29,24 +29,74 @@ return {
         "neovim/nvim-lspconfig",
         config = function()
             local lspconfig = require('lspconfig')
-            lspconfig.lua_ls.setup {}
-            lspconfig.tsserver.setup {}
-            lspconfig.pyright.setup {}
-            lspconfig.rust_analyzer.setup {
-              -- Server-specific settings. See `:help lspconfig-setup`
-              settings = {
-                ['rust-analyzer'] = {},
-              },
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+            lspconfig.lua_ls.setup {
+                capabilities = capabilities
             }
-            lspconfig.gopls.setup {}
-            lspconfig.clangd.setup {}
-            lspconfig.html.setup {}
-            lspconfig.cmake.setup {}
-            lspconfig.cssls.setup {}
-            lspconfig.cssmodules_ls.setup {}
-            lspconfig.arduino_language_server.setup {}
+            lspconfig.tsserver.setup {
+                capabilities = capabilities
+            }
+            lspconfig.pyright.setup {
+                capabilities = capabilities
+            }
+            lspconfig.rust_analyzer.setup {
+                capabilities = capabilities,
+                settings = {
+                    ['rust-analyzer'] = {},
+                },
+            }
+            lspconfig.gopls.setup {
+                capabilities = capabilities
+            }
+            lspconfig.clangd.setup {
+                capabilities = capabilities
+            }
+            lspconfig.html.setup {
+                capabilities = capabilities
+            }
+            lspconfig.cmake.setup {
+                capabilities = capabilities
+            }
+            lspconfig.cssls.setup {
+                capabilities = capabilities
+            }
+            lspconfig.cssmodules_ls.setup {
+                capabilities = capabilities
+            }
+            lspconfig.arduino_language_server.setup {
+                capabilities = capabilities
+            }
 
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+            -- Use LspAttach autocommand to only map the following keys
+            -- after the language server attaches to the current buffer
+            vim.api.nvim_create_autocmd('LspAttach', {
+                group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+                callback = function(ev)
+                    -- Enable completion triggered by <c-x><c-o>
+                    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+                    -- Buffer local mappings.
+                    -- See `:help vim.lsp.*` for documentation on any of the below functions
+                    local opts = { buffer = ev.buf }
+                    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+                    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+                    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+                    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+                    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+                    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+                    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+                    vim.keymap.set('n', '<space>wl', function()
+                        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+                    end, opts)
+                    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+                    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+                    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+                    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+                    vim.keymap.set('n', '<space>f', function()
+                        vim.lsp.buf.format { async = true }
+                    end, opts)
+                end,
+            })
         end
     }
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,30 +1,31 @@
- return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function () 
-      local configs = require("nvim-treesitter.configs")
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
 
-      configs.setup({
-                ensure_installed = {
-    "javascript",
-    "typescript",
-    "python",
-    "html",
-    "css",
-    "json",
-    "java",
-    "c",
-    "cpp",
-    "ruby",
-    "lua",
-    "go",
-    "rust",
-    "bash",
-    "markdown"
-  },
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },  
-        })
-    end
-    }
+		configs.setup({
+			auto_install = true,
+			ensure_installed = {
+				"javascript",
+				"typescript",
+				"python",
+				"html",
+				"css",
+				"json",
+				"java",
+				"c",
+				"cpp",
+				"ruby",
+				"lua",
+				"go",
+				"rust",
+				"bash",
+				"markdown",
+			},
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
+}
