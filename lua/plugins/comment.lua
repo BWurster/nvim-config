c3f188ca15d6d108700aa64008951b6271d6fbd4
Ben Wurster <wursterben17@gmail.com>
2024-04-08 21:38:28 -0500

    comments

---
+++ lua/plugins/comment.lua
@@ -0,0 +1,13 @@
+return {
+    "numToStr/Comment.nvim",
+    opts = {
+        -- add any options here
+        padding = true,
+        sticky = true,
+        toggler = {
+            line = "gc",
+            block = "gb",
+        },
+    },
+    lazy = false,
+}
