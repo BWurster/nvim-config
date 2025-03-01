76e6b298650bda9c990d106af01afbb4e2333a66
Ben Wurster <wursterben17@gmail.com>
2025-02-28 20:47:01 -0500

    this actually fixes it

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -2,7 +2,7 @@ return {
 	"rcarriga/nvim-notify",
 	config = function()
 		require("notify").setup({
-			background_colour = "NONE",
+			background_colour = "#00000000",
 		})
 		vim.notify = require("notify")
 	end,
